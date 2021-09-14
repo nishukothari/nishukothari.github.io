ec7c91a25a0aee00f8ee7882a974e470a257a7f8
nishukothari <nishuk@umich.edu>
2021-09-13 22:22:17 -0400

    finished homeview

--- react-views/HomeView.jsx
+++ react-views/HomeView.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../public/styles/HomeView.css";
-import { Carousel, CarouselItem } from "react-bootstrap";
+import {Row, Col} from "react-bootstrap";
 
 class HomeView extends React.Component {
   constructor(props) {
@@ -9,12 +9,26 @@ class HomeView extends React.Component {
 
   render() {
     return (
-      <div>
-        <Carousel>
-          <CarouselItem></CarouselItem>
-          <CarouselItem></CarouselItem>
-          <CarouselItem></CarouselItem>
-        </Carousel>
+      <div className='Holder'>
+        <div className='Overlay'>
+            <div className='titleText'>
+                <strong>Hi! I'm Nishu Kothari.</strong>
+            </div>
+            <img src="images/profile.png" className="profPic"></img>
+            <div className="logoRow">
+                <a href="https://github.com/nishukothari" target="_blank"><img src="images/gh.png" className="logo"></img></a>
+                <a href="https://www.facebook.com/nishuk12" target="_blank"><img src="images/fb.png" className="logo"></img></a>
+                <a href="https://www.instagram.com/nishu_k/" target="_blank"><img src="images/ig.png" className="logo"></img></a>
+                <a href="https://www.linkedin.com/in/nishukothari/" target="_blank"><img src="images/li.png" className="logo"></img></a>
+            </div>
+        </div>
+
+        <div className='imageBackground backDesktop'>
+          <img className='titleImg' src='images/climbing1920.jpg'/>
+        </div>
+        <div className='imageBackground backMobile'>
+          <img className='titleImg' src='images/climbing620.jpg'/>
+        </div>
       </div>
     );
   }
